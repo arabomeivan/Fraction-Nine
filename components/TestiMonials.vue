68c776c11c9b81ec5f8d8951493bb0d9289e4f1f
arabomeivan <arabomeivan@protonmail.com>
2025-04-24 17:01:18 +0100

    pushed and fixed testimonial section

--- components/TestiMonials.vue
+++ components/TestiMonials.vue
@@ -1,89 +1,94 @@
 <template>
-    <div>
-        <div id="carousel" class="carousel slide py-5">
-            <div class="carousel-inner">
-              <div class="carousel-item d-flex justify-content-between">
-                <div class="carousel-image">
-                <img src="https://res.cloudinary.com/djtwgg6op/image/upload/v1745389307/Frame_39_1_dyrl4w.png" class="img-fluid" alt="">
-                </div>
-
-                <div class="carousel-content w-50">
-                <p class="mb-5">
-fjffkfjjk
-                </p>
-                <div class="mb-5 d-flex align-items-center gap-2">
-                    <hr class="d-inline-block flex-grow-1 my-0">
-                    <div class="text-black">Ivan</div>
+    <div class="py-5">
+        <Carousel
+        >
+            <CarouselInner>
+             <CarouselItem>
+                <div class="d-flex justify-content-between">
+                    <div class="w-50 px-2">
+                    <img src="https://res.cloudinary.com/djtwgg6op/image/upload/v1745505788/IMG_3108_vmgbv0.jpg" class="img-fluid w-50" alt="">
+                    </div>
+    
+                    <div class="content w-50">
+                    <p class="mb-5">
+                        Marshall’s creativity is unmatched. His direction during the shoot made everything effortless, and the final images looked like they belonged in Vogue
+                    </p>
+                    <div class="mb-5 d-flex align-items-center gap-2">
+                        <hr class="d-inline-block flex-grow-1 my-0">
+                        <div class="text-black">Ivan Arabome</div>
+                      </div>
+    
+                    <div class="d-flex gap-2 justify-content-end">
+                   <button type="button" class="rounded-circle align-items-center justify-content-cente number-badge btn text-black border border-black bg-transparent " data-bs-target="#carousel" data-bs-slide="prev">
+                   <BootstrapIcon name="bi bi-arrow-left"/>
+                   </button>
+                   <button type="button" class=" d-flex align-items-center justify-content-center rounded-circle number-badge btn text-black border border-black bg-transparent" data-bs-target="#carousel" data-bs-slide="next">
+                    <BootstrapIcon name="bi bi-arrow-right"/>
+                   </button>
+                    </div>
+    
+                    </div>
+    
                   </div>
-
-                <div class="d-flex gap-2 justify-content-end">
-               <button type="button" class="rounded-circle align-items-center justify-content-cente number-badge btn text-black border border-black bg-transparent " data-bs-target="#carousel" data-bs-slide="prev">
-               <BootstrapIcon name="bi bi-arrow-left"/>
-               </button>
-               <button type="button" class=" d-flex align-items-center justify-content-center rounded-circle number-badge btn text-black border border-black bg-transparent" data-bs-target="#carousel" data-bs-slide="next">
-                <BootstrapIcon name="bi bi-arrow-right"/>
-               </button>
-                </div>
-
-                </div>
-
-              </div>
-
-              <div class="carousel-item  d-flex justify-content-between">
-                <div class="carousel-image">
-                <img src="https://res.cloudinary.com/djtwgg6op/image/upload/v1745389307/Frame_39_1_dyrl4w.png" class="img-fluid" alt="">
-                </div>
-
-                <div class="carousel-content w-50">
-                <p class="mb-5">
-fjffkfjjk
-                </p>
-                <div class="mb-5 d-flex align-items-center gap-2">
-                    <hr class="d-inline-block flex-grow-1 my-0">
-                    <div class="text-black">Ivan</div>
+             </CarouselItem>
+             <CarouselItem>
+                <div class="d-flex justify-content-between">
+                    <div class="w-50 px-2">
+                    <img src="https://res.cloudinary.com/djtwgg6op/image/upload/v1745389307/Frame_39_1_dyrl4w.png" class="img-fluid" alt="">
+                    </div>
+    
+                    <div class="content w-50">
+                    <p class="mb-5">
+                        I’ve never seen a crew move this fast without sacrificing quality. They storyboarded on Monday and we had a color‑graded master by Friday.”
+                    </p>
+                    <div class="mb-5 d-flex align-items-center gap-2">
+                        <hr class="d-inline-block flex-grow-1 my-0">
+                        <div class="text-black">Helen</div>
+                      </div>
+    
+                    <div class="d-flex gap-2 justify-content-end">
+                   <button type="button" class="rounded-circle align-items-center justify-content-cente number-badge btn text-black border border-black bg-transparent " data-bs-target="#carousel" data-bs-slide="prev">
+                   <BootstrapIcon name="bi bi-arrow-left"/>
+                   </button>
+                   <button type="button" class=" d-flex align-items-center justify-content-center rounded-circle number-badge btn text-black border border-black bg-transparent" data-bs-target="#carousel" data-bs-slide="next">
+                    <BootstrapIcon name="bi bi-arrow-right"/>
+                   </button>
+                    </div>
+    
+                    </div>
+    
                   </div>
-
-                <div class="d-flex gap-2 justify-content-end">
-               <button type="button" class="rounded-circle align-items-center justify-content-cente number-badge btn text-black border border-black bg-transparent " data-bs-target="#carousel" data-bs-slide="prev">
-               <BootstrapIcon name="bi bi-arrow-left"/>
-               </button>
-               <button type="button" class=" d-flex align-items-center justify-content-center rounded-circle number-badge btn text-black border border-black bg-transparent" data-bs-target="#carousel" data-bs-slide="next">
-                <BootstrapIcon name="bi bi-arrow-right"/>
-               </button>
-                </div>
-
-                </div>
-
-              </div>
-
-              <div class="carousel-item  d-flex justify-content-between">
-                <div class="carousel-image">
-                <img src="https://res.cloudinary.com/djtwgg6op/image/upload/v1745389307/Frame_39_1_dyrl4w.png" class="img-fluid" alt="">
-                </div>
-
-                <div class="carousel-content w-50">
-                <p class="mb-5">
-fjffkfjjk
-                </p>
-                <div class="mb-5 d-flex align-items-center gap-2">
-                    <hr class="d-inline-block flex-grow-1 my-0">
-                    <div class="text-black">Ivan</div>
+             </CarouselItem>
+             <CarouselItem>
+                <div class="d-flex justify-content-between">
+                    <div class="w-50 px-2">
+                    <img src="https://res.cloudinary.com/djtwgg6op/image/upload/v1745389497/Frame_40_stcmvj.png" class="img-fluid" alt="">
+                    </div>
+    
+                    <div class="content w-50">
+                    <p class="mb-5">
+                        Our documentary short looks and sounds like something I’d stream on Netflix. Clients keep asking who produced it.
+                    </p>
+                    <div class="mb-5 d-flex align-items-center gap-2">
+                        <hr class="d-inline-block flex-grow-1 my-0">
+                        <div class="text-black">Adam Levine</div>
+                      </div>
+    
+                    <div class="d-flex gap-2 justify-content-end">
+                   <button type="button" class="rounded-circle align-items-center justify-content-cente number-badge btn text-black border border-black bg-transparent " data-bs-target="#carousel" data-bs-slide="prev">
+                   <BootstrapIcon name="bi bi-arrow-left"/>
+                   </button>
+                   <button type="button" class=" d-flex align-items-center justify-content-center rounded-circle number-badge btn text-black border border-black bg-transparent" data-bs-target="#carousel" data-bs-slide="next">
+                    <BootstrapIcon name="bi bi-arrow-right"/>
+                   </button>
+                    </div>
+    
+                    </div>
+    
                   </div>
-
-                <div class="d-flex gap-2 justify-content-end">
-               <button type="button" class="rounded-circle align-items-center justify-content-cente number-badge btn text-black border border-black bg-transparent " data-bs-target="#carousel" data-bs-slide="prev">
-               <BootstrapIcon name="bi bi-arrow-left"/>
-               </button>
-               <button type="button" class=" carousel-control-next d-flex align-items-center justify-content-center rounded-circle number-badge btn text-black border border-black bg-transparent" data-bs-target="#carousel" data-bs-slide="next">
-                <BootstrapIcon name="bi bi-arrow-right"/>
-               </button>
-                </div>
-
-                </div>
-
-              </div>
-            </div>
-          </div>
+             </CarouselItem>
+            </CarouselInner>
+           </Carousel>
     </div>
 </template>
 <script>
